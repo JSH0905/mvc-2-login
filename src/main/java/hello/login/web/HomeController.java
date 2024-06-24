a7661d2c0fd1327ddf0be29302fa3e732624c6ff
JSH0905 <2000tjdgns@naver.com>
2024-06-24 20:14:09 +0900

    쿠키 사용해보기

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,45 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    /**
+     * 로그인에 성공하면 홈화면으로 이동한다.
+     * 이때, 기존 홈 화면의 회원가입, 로그인 버튼 -> 상품관리, 로그아웃 버튼으로 변경해야한다.
+     * 이를 위해 쿠키에 저장된 memberId가 필요한데 @CookieValue를 이용해서 가져올 수 있다.
+     */
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false)Long memberId, Model model){
+        if(memberId == null){
+            return "home";
+        }
+
+        //로그인 성공
+        Member loginMember = memberRepository.findById(memberId);
+        if(loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -24,8 +27,13 @@ public class LoginController {
         return "login/loginForm";
     }
 
+    /**
+     * 기본적으로 쿠키의 사용방법을 알아봄 -> 심각한 보안문제 발생 : 클라이언트의 쿠키를 임의로 바꾸거나 탈취가 가능함.
+     * 대책 방법 : 서버 세션
+     */
+
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
 
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -39,10 +47,23 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 정상 처리 TODO
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
-
-
     }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
 }
